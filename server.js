ff09df60ea8e5f88df9e901e6024f75fa27023e8
TimW <mithgirw@gmail.com>
2016-08-03 22:59:43 +0000

    CRUD working

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var jsonParser = bodyParser.json();
 
 var Storage = function() {
@@ -40,26 +39,46 @@ app.post('/items', jsonParser, function (req, res) {
 });
 
 /** DELETE **/
-app.delete('/items/:id', function (req, res) {
+app.delete('/items/:id', jsonParser, function (req, res) {
+    // Parse the string to an integer
     var id = parseInt(req.params.id);
+    console.log(req.params);
+    // If no id is received return fail status
     if (!req.params.id) {
         return res.sendStatus(400);
     }
+    // Loop through all items in the object
+    // If the sent id matches the id in storage splice that
     for (var i = 0; i < storage.items.length; i++) {
         if (id === storage.items[i].id) {
             storage.items.splice(i, 1);
         }
     }
     console.log(req.params.id);
-    
+    // Respond with 200 and the json object storage.items
     res.status(200).json(storage.items);
 });
 
 /** PUT **/
-app.put('items/:id', function (req, res) {
-    if (!req.id) {
+app.put('/items/:id', jsonParser, function (req, res) {
+    var id = parseInt(req.params.id);
+    
+    if (!req.params.id) {
         return res.sendStatus(400);
     }
+    
+    for (var i = 0; i < storage.items.length; i++) {
+        if (id === storage.items[i].id) {
+                //removes object to be updated
+            storage.items.splice(i, 1);
+                //add new updated object + id
+            storage.items.unshift({ name: req.body.name, id: id});
+            console.log(req.body.name);
+            
+        }
+    }
+    var item = storage.add(req.body.name);
+    res.status(200).json(item);
     
 });
 
